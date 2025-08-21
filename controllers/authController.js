e6a73b4e009808d741141f03dd5b0c08a17362ca
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-21 15:55:49 +0700

    feature/ adding login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,8 @@
 const User = require('../models/User.js');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secretKey = process.env.JWT_SECRET;
 
 async function registerUser(req, res) {
     const { firstName, lastName, username, password } = req.body;
@@ -17,6 +21,33 @@ async function registerUser(req, res) {
     }
 }
 
+async function loginUser(req, res) {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username' });
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        let token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
+        let finalData = {
+            userId: user._id,
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            token
+        }
+        res.send(finalData);
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(400).json({ message: error.message });
+    }
+}
+
 module.exports = {
     registerUser,
+    loginUser
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/register', authController.registerUser);
+router.post('/login', authController.loginUser);
 
 module.exports = router;

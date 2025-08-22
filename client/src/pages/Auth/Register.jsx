b9f48f6949ea5c8b8d06ebc00912f1025f9c2888
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-22 09:53:51 +0700

    feature/ adding register UI

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
+import styles from './Login.module.css';
+import login from '../../assets/login.png';
+import { Button, Input, message } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthServices from '../../services/authServices.js';
 
 function Register() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const data = {
+        firstName,
+        lastName,
+        username,
+        password
+      }
+      const response = await AuthServices.registerUser(data);
+      console.log(response.data);
+      message.success("Registration successful! Please login.");
+      navigate("/login");
+    } catch (error) {
+      message.error("Registration failed. Please try again.");
+      console.error("Registration failed:", error);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
   return (
     <div>
-      <h1>Register</h1>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input type="email" />
+      <div className={styles.login__card}>
+        <img src={login} alt="Login" />
+        <h2>Register</h2>
+        <div className={styles.input__inline__wrapper}>
+          <Input placeholder="First Name" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          <Input placeholder="Last Name" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" />
+        <div className={styles.input__wrapper}>
+          <Input placeholder="Username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
         </div>
-        <button type="submit">Register</button>
-      </form>
+        <div className={styles.input__wrapper}>
+          <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className={styles.input__info}>
+          <div className={styles.register__link}>
+            Already have an account? <Link to="/login">Login</Link>
+          </div>
+        </div>
+        <Button type="primary" size='large' disabled={!username || !password} onClick={handleSubmit}>Register</Button>
+    </div>
     </div>
   );
 }

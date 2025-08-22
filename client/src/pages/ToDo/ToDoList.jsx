85d65b695897f323e1de216e15f745fee0f01791
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-22 17:03:39 +0700

    feat(UI): incomplete-setting UI for getting all todo tasks

--- client/src/pages/ToDo/ToDoList.jsx
+++ client/src/pages/ToDo/ToDoList.jsx
@@ -6,12 +6,48 @@ import { getErrorMessage } from "../../utils/GetError.js";
 import { getUserDetails } from "../../utils/GetUser";
 import {ToDoServices } from "../../services/toDoServices.js";
 import { get } from "mongoose";
+import { useNavigate } from 'react-router';
 
 function ToDoList() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [isAdding, setIsAdding] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [allToDo, setAllToDo] = useState([]);
+  const navigate = useNavigate();
+
+  const getAllToDo = async()=>{
+    try {
+      let user = getUserDetails();
+      console.log(user?.userId);
+      const response = await ToDoServices.getAllToDos(user?.userId);
+      console.log(response.data);
+      setAllToDo(response.data);
+    } catch (error) {
+      console.log(error);
+      message.error(getErrorMessage(error));
+    }
+  }
+
+  useEffect(()=>{
+    let user = getUserDetails();
+      const getAllToDo = async()=>{
+        try {
+          console.log(user?.userId);
+          const response = await ToDoServices.getAllToDos(user?.userId);
+          console.log(response.data);
+          setAllToDo(response.data);
+        } catch (error) {
+          console.log(error);
+          message.error(getErrorMessage(error));
+        }
+      }
+      if(user && user?.userId){
+        getAllToDo();
+      }else{
+        navigate('/login');
+      }
+    }, [navigate]);
 
   const handleSubmitTask = async () => {
     setLoading(true);
@@ -50,6 +86,23 @@ function ToDoList() {
         </div>
       </div>
       <Divider />
+
+      <div>
+        {allToDo.map((item)=>{
+          return (
+            <div key={item?._id} className={styles.toDoCard}>
+              <div>
+                <div className={styles.toDoStatus}>
+                  {item.isCompleted ? "Completed" : "Pending"}
+                </div>
+              </div>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+          )
+        })}
+      </div>
+
       <Modal confirmLoading={loading} title="Add New To Do Task" open={isAdding} onOk={handleSubmitTask} onCancel={()=>setIsAdding(false)}>
         <Input style={{marginBottom:'1rem'}} placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)} />
         <Input.TextArea placeholder='Description' value={description} onChange={(e)=>setDescription(e.target.value)} />

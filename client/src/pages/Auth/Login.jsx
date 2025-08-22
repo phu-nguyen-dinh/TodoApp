88d101de7f9dfb448f9b19467000db6d6ac20abe
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-22 09:19:22 +0700

    feature/ adding login UI

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,20 +1,53 @@
 import React from "react";
+import AuthServices from "../../services/authServices.js";
+import {Link, useNavigate } from "react-router-dom";
+import  styles from "./Login.module.css"
+import login from "../../assets/login.png";
+import {Button, Input, message} from "antd";
 
 function Login() {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async () => {
+        try{
+            setLoading(true);
+            let data ={
+                username,
+                password
+            }
+            const response = await AuthServices.loginUser(data);
+            console.log(response.data);
+            localStorage.setItem("token", response.data.token);
+            navigate("/to-do-list");
+        }catch (error) {
+            console.error("Login failed:", error);
+        }finally {
+            setLoading(false);
+        }
+    }
   return (
     <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input type="email" />
+      <div className={styles.login__card}>
+        <img src={login} alt="Login" />
+        <h2>Login</h2>
+        <div className={styles.input__wrapper}>
+          <Input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" />
+        <div className={styles.input__wrapper}>
+          <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <button type="submit">Login</button>
-      </form>
+        <div className={styles.input__info}>
+          <div className={styles.register__link}>
+            New user? <Link to="/register">Register</Link>
+          </div>
+          <div>
+            <Button loading={loading} type="primary" size="large" disabled={!username || !password} onClick={handleSubmit}>Login</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

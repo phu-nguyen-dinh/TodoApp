48d36e29186f7807bda3610762932acbfc6c0d15
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-22 14:01:28 +0700

    feat(authentication): Adding authentication middleware

---
+++ middleware/authJwt.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretKey = process.env.JWT_SECRET;
+
+exports.authenticateToken = async (req, res, next) => {
+    let token = req.header('Authorization');
+    if(!token) return res.status(401).send({message:'Authentication Failed!'});
+
+    jwt.verify(token,secretKey,(err,user)=>{
+        if(err) return res.status(403).send({message:"Token is not valid! Please Login again!"});
+        req.user = user;
+        next();
+    })
+}
--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const toDoController = require('../controllers/ToDoController');
+const authenticateToken = require('../middleware/authJwt').authenticateToken;
 
-router.post('/', toDoController.createToDo);
-router.get('/:id', toDoController.getAllToDos);
-router.put('/:id', toDoController.updateToDo);
-router.delete('/:id', toDoController.deleteToDo);
+router.post('/', authenticateToken, toDoController.createToDo);
+router.get('/:id', authenticateToken, toDoController.getAllToDos);
+router.put('/:id', authenticateToken, toDoController.updateToDo);
+router.delete('/:id', authenticateToken, toDoController.deleteToDo);
 
 module.exports = router;
